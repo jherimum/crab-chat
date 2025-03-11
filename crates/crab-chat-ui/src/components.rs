2c1ecaa5f13f97f8501a1267e40db86366642299
Jherimum <eugenio.perrottaneto@gmail.com>
2025-03-11 11:31:56 -0300

    draw methods

--- crates/crab-chat-ui/src/components.rs
+++ crates/crab-chat-ui/src/components.rs
@@ -8,7 +8,6 @@ use tokio::sync::mpsc::UnboundedSender;
 use crate::{action::Action, config::Config, tui::Event};
 
 pub mod home;
-pub mod rooms_popup;
 
 /// `Component` is a trait that represents a visual and interactive element of the user interface.
 ///
--- crates/crab-chat-ui/src/components/home/mod.rs
+++ crates/crab-chat-ui/src/components/home/mod.rs
@@ -20,6 +20,12 @@ async fn handle_peer_events(
     }
 }
 
+pub enum Focus {
+    Rooms,
+    Chat,
+    Input,
+}
+
 pub struct Home {
     peer: Peer,
     command_tx: Option<UnboundedSender<Action>>,
@@ -41,6 +47,44 @@ impl Home {
             peer,
             show_rooms: false,
         }
+    }
+}
+
+impl Home {
+    fn draw_chat(&mut self, frame: &mut Frame, area: Rect) {
+        frame.render_widget(
+            Block::default()
+                .borders(Borders::ALL)
+                .bg(Color::Black)
+                .title("Chat")
+                .title_alignment(Alignment::Center)
+                .title_style(Style::default().fg(Color::Green)),
+            area,
+        );
+    }
+
+    fn draw_header(&mut self, frame: &mut Frame, area: Rect) {
+        frame.render_widget(
+            Paragraph::new("Crab Chat")
+                .alignment(Alignment::Center)
+                .bold()
+                .block(Block::bordered())
+                .black()
+                .bg(Color::Green),
+            area,
+        );
+    }
+
+    fn draw_rooms(&mut self, frame: &mut Frame, area: Rect) {
+        frame.render_widget(
+            Block::default()
+                .borders(Borders::ALL)
+                .bg(Color::Black)
+                .title("My Rooms")
+                .title_alignment(Alignment::Center)
+                .title_style(Style::default().fg(Color::Green)),
+            area,
+        );
     }
 }
 
@@ -89,66 +133,10 @@ impl Component for Home {
     fn draw(&mut self, frame: &mut Frame, area: Rect) -> Result<()> {
         let (header, main, footer) = vertical_layout(area);
         let (rooms, chat_panel) = horizontal_layout(main);
-        let (my_rooms, network_rooms) = rooms_panel(rooms);
         let (chat, input, participants) = chat_layout(chat_panel);
-
-        frame.render_widget(
-            Paragraph::new("Crab Chat")
-                .alignment(Alignment::Center)
-                .bold()
-                .block(Block::bordered())
-                .black()
-                .bg(Color::Green),
-            header,
-        );
-
-        frame.render_widget(
-            Block::default()
-                .borders(Borders::ALL)
-                .bg(Color::Black)
-                .title("My Rooms")
-                .title_alignment(Alignment::Center)
-                .title_style(Style::default().fg(Color::Green)),
-            my_rooms,
-        );
-
-        frame.render_widget(
-            Block::default()
-                .borders(Borders::ALL)
-                .bg(Color::Black)
-                .title("Network Rooms")
-                .title_alignment(Alignment::Center)
-                .title_style(Style::default().fg(Color::Green)),
-            network_rooms,
-        );
-
-        frame.render_widget(
-            Block::default()
-                .borders(Borders::ALL)
-                .bg(Color::Black)
-                .title("Chat")
-                .title_alignment(Alignment::Center)
-                .title_style(Style::default().fg(Color::Green)),
-            chat_panel,
-        );
-
-        // frame.render_widget(
-        //     Block::new()
-        //         .borders(Borders::ALL)
-        //         .green()
-        //         .style(Style::default())
-        //         .bg(Color::Black),
-        //     inner[0],
-        // );
-
-        // frame.render_widget(
-        //     Paragraph::new(self.text.as_str())
-        //         .block(Block::new().borders(Borders::ALL).black())
-        //         .style(Style::default().bg(Color::Green).fg(Color::White)),
-        //     outer[2],
-        // );
-
-        // if self.show_rooms {}
+        self.draw_header(frame, header);
+        self.draw_rooms(frame, rooms);
+        self.draw_chat(frame, chat_panel);
 
         Ok(())
     }
@@ -171,18 +159,6 @@ fn horizontal_layout(area: Rect) -> (Rect, Rect) {
     let areas = Layout::default()
         .direction(Direction::Horizontal)
         .constraints(vec![Constraint::Percentage(20), Constraint::Min(1)])
-        .split(area);
-
-    (areas[0], areas[1])
-}
-
-fn rooms_panel(area: Rect) -> (Rect, Rect) {
-    let areas = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(vec![
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
-        ])
         .split(area);
 
     (areas[0], areas[1])
